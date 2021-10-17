40e6ae5dfcdb0a61690f6ab184fcaf578197ee50
brandonxchang <brandon.xan.chang@gmail.com>
2021-10-17 04:19:32 -0500

    Convert tslint to eslint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import serveIndex from 'serve-index';
-import youtubedl, { YtResponse } from 'youtube-dl-exec';
+import youtubedl from 'youtube-dl-exec';
 import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
@@ -10,8 +10,8 @@ const SAVE_DIRECTORY = process.env.SAVE_DIRECTORY;
 const OUTPUT_FORMAT = process.env.OUTPUT_FORMAT;
 const AUDIO_ONLY = process.env.AUDIO_ONLY === 'true';
 
-function createDLFlags(fileName: string = null, jsonDump: boolean = false) {
-    const flags: any = {
+const createDLFlags = (fileName: string = null, jsonDump = false) => {
+    const flags: Record<string, string | boolean | number> = {
         noWarnings: true,
         noCallHome: true,
         noCheckCertificate: true,
@@ -33,20 +33,20 @@ function createDLFlags(fileName: string = null, jsonDump: boolean = false) {
     return flags;
 }
 
-async function getVideoJsonInfo(url: string) {
+const getVideoJsonInfo = async (url: string) => {
     return youtubedl(url, createDLFlags(null, true));
 }
 
 app.use((req, res, next) => {
-    // tslint:disable-next-line:no-console
+    // eslint-disable-next-line no-console
     // console.log(req);
-    // tslint:disable-next-line:no-console
+    // eslint-disable-next-line no-console
     console.log('Time: ', Date.now());
     next();
 });
 
 app.use('/request-type', (req, res, next) => {
-    // tslint:disable-next-line:no-console
+    // eslint-disable-next-line no-console
     console.log('Request type: ', req.method);
     next();
 });
@@ -60,47 +60,59 @@ app.get('/', (req, res) => {
 });
 
 app.post('/format', (req, res) => {
+    const body = req.body as { url: string };
     const flags = createDLFlags(null, false);
     flags.listFormats = true;
 
-    youtubedl(req.body.url, flags)
-        // tslint:disable-next-line:no-console
+    youtubedl(body.url, flags)
+        // eslint-disable-next-line no-console
         .then(output => console.log(output))
-        // tslint:disable-next-line:no-console
+        // eslint-disable-next-line no-console
         .catch(output => console.log(output));
 
     res.status(200).send('Success');
 });
 
-app.post('/videojson', async (req, res) => {
-    const output = await getVideoJsonInfo(req.body.url);
+app.post('/videojson', (req, res) => {
+    (async () => {
+        const body = req.body as { url: string };
+        const output = await getVideoJsonInfo(body.url);
 
-    // tslint:disable-next-line:no-console
-    console.log(output.title);
+        // eslint-disable-next-line no-console
+        console.log(output.title);
 
-    res.status(200).send(output);
+        res.status(200).send(output);
+    })()
+        // eslint-disable-next-line no-console
+        .catch(e => console.log(e));
+
 });
 
-app.post('/download', async (req, res) => {
-    const vidInfo = await getVideoJsonInfo(req.body.url);
-    const generatedId = uuidv4();
-    const fileName = vidInfo.title.replace(/[\/\\><|?:"*]/g, ' ');
-    const downloadedItem = `${FOLDER_LOCATION}${SAVE_DIRECTORY}${fileName}.mp3`;
+app.post('/download', (req, res) => {
+    (async () => {
+        const body = req.body as { url: string };
+        const vidInfo = await getVideoJsonInfo(body.url);
+        const generatedId = uuidv4();
+        const fileName = vidInfo.title.replace(/[\/\\><|?:"*]/g, ' ');
+        const downloadedItem = `${FOLDER_LOCATION}${SAVE_DIRECTORY}${fileName}.mp3`;
 
-    // tslint:disable-next-line:no-console
-    console.log(`Request to download ${vidInfo.title} at ${req.body.url} as ${downloadedItem}`);
-    youtubedl(req.body.url, createDLFlags(fileName))
-        .then(async output => {
-            // tslint:disable-next-line:no-console
-            console.log(output);
-            // tslint:disable-next-line:no-console
-            console.log(`Downloaded to: ${downloadedItem}`);
-        })
-        // tslint:disable-next-line:no-console
-        .catch(output => console.log(output));
+        // eslint-disable-next-line no-console
+        console.log(`Request to download ${vidInfo.title} at ${body.url} as ${downloadedItem}`);
+        youtubedl(body.url, createDLFlags(fileName))
+            .then(output => {
+                // eslint-disable-next-line no-console
+                console.log(output);
+                // eslint-disable-next-line no-console
+                console.log(`Downloaded to: ${downloadedItem}`);
+            })
+            // eslint-disable-next-line no-console
+            .catch(output => console.log(output));
 
-    res.status(200).send({ id: generatedId });
+        res.status(200).send({ id: generatedId });
+    })()
+        // eslint-disable-next-line no-console
+        .catch(e => console.log(e));
 });
 
-// tslint:disable-next-line:no-console
+// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Example app is listening on port ${PORT}.`));
